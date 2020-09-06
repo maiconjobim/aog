3a561db7308692f439c8813b3daa3f5e8ed739d2
maicon-jobim <maicon.jobim@sequor.com.br>
2020-09-06 19:08:09 -0300

    feat MoneyMasked and TextFormField

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:aog/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,10 +23,43 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var _gasController = new MoneyMaskedTextController();
+
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: ListView(children: [
         Logo(),
+        Row(
+          children: [
+            Container(
+              width: 100,
+              alignment: Alignment.centerRight,
+              child: Text(
+                "Gasolina",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontFamily: "Big Shouders Display"),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: TextFormField(
+                controller: _gasController,
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontFamily: "Big Shouders Display"),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ],
+        )
       ]),
     );
   }
